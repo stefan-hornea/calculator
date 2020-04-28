1128af1ef08c8c3858c72a3d57499f9197bea0df
stefan-hornea <42001620+stefan-hornea@users.noreply.github.com>
2020-04-28 21:47:40 +0300

    Added comment to explain unary addition

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,7 +11,8 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return a - b },
+    //the unary plus operator is used to convert the variables to numbers, from string
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
